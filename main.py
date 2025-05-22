18ab18de380f1cf2078d718a5ef3056f73fd3bb9
EduardoZenDev <lalozen68@gmail.com>
2025-05-22 12:27:29 -0300

    Mi microservicio en Python 2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return '¡Hola desde Flask!'
+    return "¡Hola desde Render y Flask!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
