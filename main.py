c99438c2a3f1bbb5356b8847ad775d7efa719bdb
EduardoZenDev <lalozen68@gmail.com>
2025-05-22 12:50:53 -0300

    Proyecto listo para Render 2

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def home():
     return "¡Hola desde Render y Flask!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render usará este puerto
     app.run(host="0.0.0.0", port=port)
